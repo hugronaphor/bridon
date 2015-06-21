cf12eb3d28c43cf1a70b9120b8fd432e23fbf124
Cornel Andreev <hugronaphor@gmail.com>
2015-06-21 08:08:12 +0100

    Entity extractor implementation.

--- sites/all/modules/custom/news_core/theme/news-main-list.tpl.php
+++ sites/all/modules/custom/news_core/theme/news-main-list.tpl.php
@@ -6,5 +6,23 @@
     <div class="summary">
       <?php print $result['summary']; ?>
     </div>
+    <?php if (!empty($result['more_entities'])) : ?>
+      <div class="more-like-this">
+        <a class="more-like-this-button" href="#">More about this</a>
+        <div class="hidden expand-entities">
+          <?php foreach ($result['more_entities'] as $entity_name => $more_entity): ?>
+            <div class="term-search">
+              <div class="entity-wrapp-label"><?php print $entity_name; ?></div>
+              <div class="entity-wrapp-expand hidden">
+                <?php foreach ($more_entity as $final_term): ?>
+                  <a class="final-term" href="/?search=<?php print $final_term; ?>"><?php print $final_term; ?></a>
+                <?php endforeach; ?>
+              </div>
+            </div>
+
+          <?php endforeach; ?>
+        </div> 
+      </div>
+    <?php endif; ?>
   </article>
 <?php endforeach; ?>
--- sites/all/themes/news/js/main.js
+++ sites/all/themes/news/js/main.js
@@ -1,13 +1,29 @@
-(function ($) {
+(function($) {
 
   'use strict';
 
   /**
    * A simple Drupal behavior example.
    */
-  Drupal.behaviors.cirlceThemeExample = {
-    attach: function (context) {
-      // Your code goes here.
+  Drupal.behaviors.moreLikeThis = {
+    attach: function(context) {
+
+      $('.more-like-this-button').on("click", function() {
+        var btn = $(this);
+        var parentWrapper = btn.parents('article');
+        
+        btn.slideUp(0, function() {
+          parentWrapper.find('.expand-entities').slideDown(200);
+        });
+        return false;
+      });
+
+      $('.entity-wrapp-label').on("click", function() {
+        var parentWrapper = $(this).parents('.term-search');
+        parentWrapper.find('.entity-wrapp-expand').slideDown(200);
+        return false;
+      });
+
     }
   };
 
